c25d3fafccbfbb6afdca17fcc42067660a22cb09
Andreas Rohrbach <skuddeliwoo@gmail.com>
2021-08-16 16:55:10 +0200

    update port

--- src/app/index.js
+++ src/app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express();
-const port = 3001;
+const port = 5678;
 
 app.use(express.static(path.resolve(__dirname, 'public')))
 
