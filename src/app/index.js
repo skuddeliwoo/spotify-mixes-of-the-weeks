14ea699c8298f2e6029ad6f798759bc0d13e5ed7
Andreas Rohrbach <skuddeliwoo@gmail.com>
2022-01-04 17:59:31 +0100

    revert proxy changes

--- src/app/index.js
+++ src/app/index.js
@@ -10,11 +10,16 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/public/index.html')
 })
 
+app.get('/playlists', (req, res) => {
+	res.sendFile(__dirname + '/public/playlists.html')
+})
+
 app.get('/login', (req, res) => {
 	const clientId = '57ae3c5aa90a4cd1a56af19f82fc5657'
 	const scopes = 'playlist-modify-private playlist-modify-public playlist-read-private playlist-read-collaborative'
+	// const redirect_uri = 'h2947445.stratoserver.net:5678/playlists'
 	const redirect_uri = 'http://' + req.hostname + ':' + port + '/playlists.html'
-	// TODO: change 'http://' to actual used protocol of express
+	console.log(redirect_uri);
 	res.redirect('https://accounts.spotify.com/authorize'
 		+ '?response_type=token'
 		+ '&client_id=' + clientId
